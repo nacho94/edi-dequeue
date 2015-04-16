62e539bd0982698a216e20312ae4ceb02638b28a
Nacho Crespo <ignacio.crespo.martinez@gmail.com>
2015-04-16 19:49:35 +0200

    implementado enqueue

--- src/ule/edi/dequeue/RingBufferDeQueueImpl.java
+++ src/ule/edi/dequeue/RingBufferDeQueueImpl.java
@@ -1,19 +1,40 @@
 package ule.edi.dequeue;
 
+import java.util.Arrays;
+
 import ule.edi.EmptyCollectionException;
 
 public class RingBufferDeQueueImpl<E> implements DeQueue<E> {
-
+	private final int RESERVATION_SIZE = 5;
+	private E[] array = (E[]) new Object[RESERVATION_SIZE];
+	private int arraySize = 0;
+	private int rear = 0;
+	
+	private void reserveMemory() {
+		if(arraySize <= rear) {
+			// no se pueden crear arrays de objetos sin tipo de ahi esto
+			
+			array = Arrays.copyOf(array, arraySize + RESERVATION_SIZE);
+			arraySize += RESERVATION_SIZE;
+			for(int i=rear; i<arraySize; i++) {
+				array[i] = null;
+			}
+		}
+	}
+	
 	@Override
 	public void enqueueRear(E element) {
-		// TODO Auto-generated method stub
-		
+		reserveMemory();
+		array[rear] = element;
+		rear++;
 	}
 
 	@Override
 	public void enqueueFront(E element) {
-		// TODO Auto-generated method stub
-		
+		reserveMemory();
+		System.arraycopy(array, 0, array, 1, rear);
+		array[0] = element;
+		rear++;
 	}
 
 	@Override
